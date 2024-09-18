7adc8064ba79108fe42a87bf2924ee8dd8444d75
andreac324 <terisa32873@gmail.com>
2024-09-17 23:50:19 -0700

    Initial Commit

---
+++ ca14/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ca14
+//
+//  Created by Andrea d Cazares on 9/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
